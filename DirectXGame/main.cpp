9ced8a7c0539912d433cb5492ae8ca15a1000c6b
mano <k025g0019@g.neec.ac.jp>
2026-01-24 22:43:46 +0900

    comit

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,61 @@
+#include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
+
+using namespace KamataEngine;
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	KamataEngine::Initialize(L"LC1B_29_マノ_ショウタ_AL2");
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+
+	// ゲームシーンの初期化
+
+	gameScene->Initialize();
+
+	while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		//
+		//
+		//
+
+		//================================================
+		// ゲームシーンの更新
+		//================================================
+		gameScene->Update();
+
+		//================================================
+		// 描画開始
+		//================================================
+
+		dxCommon->PreDraw();
+
+		//================================================
+		// ゲームシーンの描画
+		//==============================================
+
+		gameScene->Draw();
+
+		//================================================
+		// 描画終了
+		//================================================
+		dxCommon->PostDraw();
+	}
+
+	// ゲームシーンの解放
+	delete gameScene;
+
+	// nullptrの代入
+	gameScene = nullptr;
+
+	KamataEngine::Finalize();
+
+	return 0;
+}
