7610e2cacd40d182d00f5136f40192667a6fc538
mano <k025g0019@g.neec.ac.jp>
2026-01-30 13:00:05 +0900

    AL2_04_03

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,10 +2,57 @@
 
 using namespace KamataEngine;
 void GameScene::Initialize() {
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	// スプライトインスタンスの生成
+	model_ = Model::Create();
+
+	worldTransform_.Initialize();
+	camera_.Initialize();
+
+	PrimitiveDrawer::GetInstance()->SetCamera(&camera_);
+
+	debugCamera_ = new DebugCamera(1280, 720);
+
+	// 軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+
+	// 軸方向表示が参照するビュープロジェクションを指定する
+	AxisIndicator::GetInstance()->SetTargetCamera(&debugCamera_->GetCamera());
+
 }
 
 void GameScene::Update() {
+
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::End();
+
+	debugCamera_->Update();
+	//// スプライトの座標を取得
+	// Vector2 position = sprite_->GetPosition();
+
+	//// 座標を{2.0f, 1.0f}移動
+	// position.x += 2.0f;
+	// position.y += 1.0f;
+
+	//// 移動した座標をスプライトに反映
+	// sprite_->SetPosition(position);
 }
 
 void GameScene::Draw() {
+
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
+
+	Model::PreDraw();
+	model_->Draw(worldTransform_, debugCamera_->GetCamera(), textureHandle_);
+	Model::PostDraw();
+}
+
+GameScene::GameScene() { Initialize(); }
+
+GameScene::~GameScene() {
+	delete debugCamera_;
+	delete model_;
+	sprite_ = nullptr;
 }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -4,6 +4,27 @@
 class GameScene {
 
 public:
+	uint32_t textureHandle_ = 0; // テクスチャハンドル
+
+	// 3Dモデルのハンドル
+	KamataEngine::Model* model_ = nullptr;
+
+	// スプライト
+	KamataEngine::Sprite* sprite_ = nullptr;
+
+	KamataEngine::WorldTransform worldTransform_;
+
+	KamataEngine::Camera camera_;
+
+	KamataEngine::DebugCamera* debugCamera_ = nullptr;
+
+
+	
+
+	GameScene();
+
+	~GameScene();
+
 	// 初期化
 	void Initialize();
 
@@ -12,5 +33,4 @@ public:
 
 	// 描画
 	void Draw();
-
 };
